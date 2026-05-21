378a1d0cc5305d59d6dc4dce9fc42b50429750b3
Pipe1425 <pipezam2005@gmail.com>
2026-05-20 23:17:45 -0500

    Service Completos clases reserva nacional e internacional
    Se crearon los servicios para los dos tipos de reserva y ademas se corrigieron los atributos metodo de transporte ya que tenian un metodo String

--- TravelX_Back/src/main/java/co/edu/unbosque/travelx/dto/ReservaInternacionalDTO.java
+++ TravelX_Back/src/main/java/co/edu/unbosque/travelx/dto/ReservaInternacionalDTO.java
@@ -3,6 +3,8 @@ package co.edu.unbosque.travelx.dto;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import co.edu.unbosque.travelx.entity.Reserva.MetodoTransporte;
+
 public class ReservaInternacionalDTO extends ReservaDTO{
 	
 	private String paisOrigen;
@@ -17,14 +19,14 @@ public class ReservaInternacionalDTO extends ReservaDTO{
 		// TODO Auto-generated constructor stub
 	}
 	
-	public ReservaInternacionalDTO(Long personaId, String metodoTransporte, LocalDate fechaInicio, LocalDate fechaFin,
+	public ReservaInternacionalDTO(Long personaId, MetodoTransporte metodoTransporte, LocalDate fechaInicio, LocalDate fechaFin,
 			String ciudadOrigen, String ciudadDestino, double precioTransporte, String hotel, double precioHospedaje) {
 		super(personaId, metodoTransporte, fechaInicio, fechaFin, ciudadOrigen, ciudadDestino, precioTransporte, hotel,
 				precioHospedaje);
 		// TODO Auto-generated constructor stub
 	}
 
-	public ReservaInternacionalDTO(Long personaId, String metodoTransporte, LocalDate fechaInicio, LocalDate fechaFin,
+	public ReservaInternacionalDTO(Long personaId, MetodoTransporte metodoTransporte, LocalDate fechaInicio, LocalDate fechaFin,
 			String ciudadOrigen, String ciudadDestino, double precioTransporte, String hotel, double precioHospedaje,
 			String paisOrigen, String ciudadOrigen2, String paisDestino, String ciudadDestino2, boolean requiereVisa) {
 		super(personaId, metodoTransporte, fechaInicio, fechaFin, ciudadOrigen, ciudadDestino, precioTransporte, hotel,
---
+++ TravelX_Back/src/main/java/co/edu/unbosque/travelx/repository/ReservaInternacionalRepository.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque.travelx.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.edu.unbosque.travelx.entity.ReservaInternacional;
+
+
+public interface ReservaInternacionalRepository extends CrudRepository<ReservaInternacional, Long>{
+
+	
+
+
+
+}
